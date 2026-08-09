c6eec80c56582769c9ed05749d3e652d06c27276
aymenbedoui <aymen.bedoui@quanteam.fr>
2026-08-09 13:27:19 +0200

    Add YahooFinanceClient to ingestion init file

--- src/market_data_insights_api/ingestion/__init__.py
+++ src/market_data_insights_api/ingestion/__init__.py
@@ -1,5 +1,14 @@
 """Market data ingestion package."""
 
 from market_data_insights_api.ingestion.schemas import MarketDataAsset, MarketDataPrice
+from market_data_insights_api.ingestion.yahoo_finance_client import (
+    YahooFinanceClient,
+    YahooFinanceMarketData,
+)
 
-__all__ = ["MarketDataAsset", "MarketDataPrice"]
+__all__ = [
+    "MarketDataAsset",
+    "MarketDataPrice",
+    "YahooFinanceClient",
+    "YahooFinanceMarketData",
+]
